f943eaf99b40a8b769bcdee4d772d8b961a560ce
Monika <monika.m.gaik@gmail.com>
2017-11-20 15:54:04 +0100

    beee

--- src/main/java/pl/SpringStore/controllers/OrderController.java
+++ src/main/java/pl/SpringStore/controllers/OrderController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import pl.SpringStore.models.OrderModel;
+import pl.SpringStore.models.ProductModel;
 import pl.SpringStore.repositories.CartRepository;
 import pl.SpringStore.repositories.OrderCRUDRepository;
+import pl.SpringStore.services.ProductService;
 
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,11 +28,14 @@ import java.util.Map;
 public class OrderController {
 
     @Autowired
-    CartRepository fakeOrder;
+    CartRepository cartRepository;
     @Autowired
     OrderCRUDRepository orderCRUDRepository;
     @Autowired
     OrderModel orderModel;
+
+    @Autowired
+    ProductService productService;
 
     List<String> orderList = new ArrayList<>();
 
@@ -38,33 +43,34 @@ public class OrderController {
 
     @GetMapping("/order")
     // @ResponseBody
-    public String order(HttpServletRequest request, ModelMap modelMap) {
-        HttpSession session = request.getSession();
+    public String order(HttpServletRequest request,HttpSession session, ModelMap modelMap) {
+        session = request.getSession();
 
-        modelMap.addAttribute( "imie", fakeOrder.userOrder );//session.getAttribute( "sessionName" ) );
-        modelMap.addAttribute( "list", fakeOrder.orderProd );
-        System.out.println( fakeOrder.orderProd );
+        modelMap.addAttribute( "imie", cartRepository.userOrder );//session.getAttribute( "sessionName" ) );
+        modelMap.addAttribute( "list", cartRepository.orderProd );
+        System.out.println( "Order " + cartRepository.orderProd );
 
         return "order";
     }
 
     @GetMapping("/order/{id}")
     public String remove(@PathVariable String id, ModelMap modelMap) {
-        orderList.remove( id );
+        orderList.remove( productService.findByProductId(Integer.parseInt(id )) );
         orderList.remove( sessionHash.get( "sessionName" ) );
-        fakeOrder.orderProd.remove( id );
-        fakeOrder.userOrder = sessionHash.get( "sessionName" );
-        System.out.println( fakeOrder.orderProd );
+        System.out.println(productService.findByProductId( Integer.parseInt(id )));
+        System.out.println(cartRepository.removeById(( Integer.parseInt(id ))));
+        cartRepository.userOrder = sessionHash.get( "sessionName" );
+        System.out.println( "Remove " + cartRepository.orderProd );
 
 
-        return "order";
+        return "redirect:/order";
     }
 
 
     @GetMapping("/order/cart")
     public String order(ModelMap modelMap) {
-        
-        for (String t : fakeOrder.orderProd) {
+
+        for (ProductModel t : cartRepository.orderProd) {
             orderCRUDRepository.save( orderModel );
             System.out.println( orderModel);
         }
--- src/main/java/pl/SpringStore/repositories/CartRepository.java
+++ src/main/java/pl/SpringStore/repositories/CartRepository.java
@@ -2,6 +2,7 @@ package pl.SpringStore.repositories;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+import pl.SpringStore.models.ProductModel;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -11,27 +12,35 @@ import java.util.List;
  * Created by monik on 04.11.2017.
  */
 
-@Repository
+//@Repository
 @Component
-    public class CartRepository {
-        Long cartId;
-        public String userOrder;
-        public List<String> orderProd = new ArrayList<String>();
-        public int account;
+public class CartRepository {
+    Long cartId;
+    public String userOrder;
+    public List<ProductModel> orderProd = new ArrayList<>();
+    public int account;
 
-        public BigDecimal totalprice;
+    public BigDecimal totalprice;
 
-        private void add(String id){
-            orderProd.add( id );// dodawanie do koszyka
-        }
+    private void add(ProductModel id){
+        orderProd.add( id );// dodawanie do koszyka
+    }
 
-        private void remove(String product){
-            orderProd.remove( product ); // usuwanie z koszyka
-        }
-        private void setAccount(){
-
-        }
-
+    private void remove(ProductModel product){
+        orderProd.remove( product ); // usuwanie z koszyka
+    }
+    private void setAccount(){
 
     }
+
+    public boolean removeById(Integer id){
+        for(ProductModel productModel : orderProd){
+            if(productModel.getProductId()==id){
+                return orderProd.remove( productModel );
+            }
+        }
+        return false;
+    }
+
+}
 
